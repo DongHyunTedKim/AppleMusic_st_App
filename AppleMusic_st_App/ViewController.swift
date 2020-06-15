38bebc7a132b60b8e8ae1c02fbca743c13793b41
DongHyunTedKim <53430924+DongHyunTedKim@users.noreply.github.com>
2020-06-15 17:45:19 +0900

    Initial Commit

---
+++ AppleMusic_st_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMusic_st_App
+//
+//  Created by Ted on 2020. 6. 15..
+//  Copyright © 2020년 com.ted. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
